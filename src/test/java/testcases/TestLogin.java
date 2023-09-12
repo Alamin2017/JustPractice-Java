aaaa079c3246dcd26265c7831c7c20049221e278
Al-Amin <alamincse12@gmail.com>
2023-09-12 20:48:46 +0600

    pom.xml version is updated

--- src/test/java/testcases/TestLogin.java
+++ src/test/java/testcases/TestLogin.java
@@ -4,12 +4,15 @@ import testdata.DataBuilder;
 import envsetup.BaseEnv;
 import org.testng.annotations.Test;
 import pageobject.LoginPage;
+import testdata.LoginData;
+
 public class TestLogin extends BaseEnv {
     @Test(retryAnalyzer = Retry.class)
     public void LoginTest() throws InterruptedException {
         driver.get("http://teststore.automationtesting.co.uk/");
         LoginPage.clickSignInButton();
-        LoginPage.enterEmail(DataBuilder.getData().getEmail());
+        //LoginPage.enterEmail(DataBuilder.getData().getEmail());
+        LoginPage.enterEmail(LoginData.userEmail);
         Thread.sleep(5000);
     }
 }
---
+++ src/test/java/testdata/LoginData.java
@@ -0,0 +1,6 @@
+package testdata;
+
+public class LoginData {
+    public static String userEmail="alamincse321@gmail.com";
+
+}
